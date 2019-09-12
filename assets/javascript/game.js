02933aab896bf432988c1b2dee564388b87275a4
Kevin Halstead <mrmichael423@icloud.com>
2019-09-11 18:58:04 -0600

    added images and some function to the crystal game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,17 @@
 var randomScore = 50;
-var userScore;
+var userScore = 0;
 var wins = 0;
 var losses = 0;
 
-$(".crystalImg").on("click", function() {
-    userScore += 10;
+$(".crystalImg1").on("click", function() {
+    userScore +=10;
+    alert("your score: " + userScore)
+    if (userScore === randomScore) {
+        alert("YOU WIN!");
+    } else if (userScore >= randomScore) {
+        alert("YOU LOOSE!");
+
+    }
 })
 
 $("#randomScore").text(randomScore)
