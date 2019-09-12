e114c22e74f71e0117c126e6054c054de094e103
Kevin Halstead <mrmichael423@icloud.com>
2019-09-11 18:11:32 -0600

    updated functions in js and added classes in html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,10 @@
-var randomScore
-var userScore
+var randomScore = 50;
+var userScore;
 var wins = 0;
 var losses = 0;
-var crystals 
+
+$(".crystalImg").on("click", function() {
+    userScore += 10;
+})
+
+$("#randomScore").text(randomScore)
