51bbbcc75df2643474cdf38b8c17de6b672d0a81
Kevin Halstead <mrmichael423@icloud.com>
2019-09-11 20:37:42 -0600

    updated most java script un able to get pics to show on page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,13 +2,27 @@ var randomScore = 50;
 var userScore = 0;
 var wins = 0;
 var losses = 0;
+var crystalImages = ["assets/images/crystal1.jpg", "assets/images/crystal2.jpg", "assets/images/crystal3.jpg", "assets/images/cryastal4.jpg"]
 
-$(".crystalImg1").on("click", function() {
-    userScore +=10;
-    alert("your score: " + userScore)
-    if (userScore === randomScore) {
+for (var i = 0; i < 4; i++) {
+    var crystalPics = $("<img>");
+    crystalPics.addClass("crystalImg");
+    crystalPics.attr("scr", crystalImages[i]);
+    crystalPics.attr("data-value", Math.floor(Math.random() * 13 ));
+    $("#crystals").append(crystalPics);
+
+}
+
+$(".crystalImg").on("click", function() {
+    //grabs the value for each of the crystals
+    var crystalVal = ($(this).attr("data-value"));
+    //turns crystalVal from string to number so it can be added
+    crystalVal = parseInt(crystalVal)
+    
+        alert("your score: " + userScore)
+        if (userScore === randomScore) {
         alert("YOU WIN!");
-    } else if (userScore >= randomScore) {
+        } else if (userScore > randomScore) {
         alert("YOU LOOSE!");
 
     }
