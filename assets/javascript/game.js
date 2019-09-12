68bb412a9baa6883b519fee2cd55ce9ad97f177b
Kevin Halstead <mrmichael423@icloud.com>
2019-09-11 21:03:47 -0600

    add text to the page from the javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,16 @@
-var randomScore = 50;
+var randomScore = Math.floor(Math.random() *129) + 19;
 var userScore = 0;
 var wins = 0;
 var losses = 0;
+//var for crystals in an array
 var crystalImages = ["assets/images/crystal1.jpg", "assets/images/crystal2.jpg", "assets/images/crystal3.jpg", "assets/images/cryastal4.jpg"]
 
+//for loop to randomize crystal values and put them on the page
 for (var i = 0; i < 4; i++) {
     var crystalPics = $("<img>");
     crystalPics.addClass("crystalImg");
     crystalPics.attr("scr", crystalImages[i]);
-    crystalPics.attr("data-value", Math.floor(Math.random() * 13 ));
+    crystalPics.attr("data-value", Math.floor(Math.random() * 13 ) + 1);
     $("#crystals").append(crystalPics);
 
 }
@@ -18,14 +20,21 @@ $(".crystalImg").on("click", function() {
     var crystalVal = ($(this).attr("data-value"));
     //turns crystalVal from string to number so it can be added
     crystalVal = parseInt(crystalVal)
+
+    userScore += crystalVal
     
-        alert("your score: " + userScore)
+        // alert("your score: " + userScore)
         if (userScore === randomScore) {
+            wins++
         alert("YOU WIN!");
         } else if (userScore > randomScore) {
+            losses++
         alert("YOU LOOSE!");
 
     }
+    $("#yourScore").text(userScore);
+    $("#wins").text(wins);
+    $("#losses").text(losses);
 })
 
-$("#randomScore").text(randomScore)
+$("#randomScore").text(randomScore);
